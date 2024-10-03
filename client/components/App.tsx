f5a6ee44eaff515bfab16fc8bcd45394d239113a
Danica Taylor <dctaylor101@gmail.com>
2024-10-04 12:29:53 +1300

    Created some stuff

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,18 +1,21 @@
 function App() {
   return (
-    <div>
-      <header>
-        <h1>My Dev Career Plan</h1>
-        <p>React development has begun!</p>
-      </header>
-      <main>
+    <>
+      <div>
+        <header>
+          <h1>My Dev Career Plan</h1>
+          <p>React development has begun!</p>
+        </header>
+        <main>
 
-      </main>
-      <footer>
-        Created By Danica Taylor
-      </footer>
+        </main>
+        <footer>
+          Created By Danica Taylor
+        </footer>
+      </div>
+    
+    </>
 
-    </div>
   )
 }
 
